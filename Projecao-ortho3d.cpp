a9b2dbbb39d41b1bf95bf71d6d26da5bdeadcbd1
Matheus Rodrigues <matheus.rodrisantos@bol.com.br>
2019-04-08 22:13:59 -0300

    2 versão do código

--- Projecao-ortho3d.cpp
+++ Projecao-ortho3d.cpp
@@ -1,21 +1,32 @@
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
 #include <stdio.h>
 
-void Desenha(){
+void Desenha()
+{
 	
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+	//camera que mostra o cenario 	
+	gluLookAt(0.0,0.0,-5.0,//posicao da camera 
+			  0.0,0.0,0.0,//ponto onde a camera vai apontar
+			  0.0,1.0,0.0);//topo do cenario 
 	glColor3f(1.0f,1.0f,1.0f);
 	glutWireCube(2);
+	glutWireSphere(0.5,100,100);
 	glFlush();
 }
 
 
-void Inicializa(){
+void Inicializa()
+{
 	glClearColor(0.0,0.0,0.0,0.0);
-	glOrtho(-2.0,2.0,-2.0,2.0,-1.5,1.5);
 	
+	//esses são as perspectivas  das acameras 
+	 glOrtho(-2.0,2.0,-2.0,2.0,-1.5,1.5);
 	
+	//glFrustum(-1.0,1.0,-1.0,1.0,1.0,20.0);//
+	//gluPerspective(43,//angulo em Y 
+	//1.33,//proporção em X
+	//.1,20);//distancia minima e maxima no z 
 }
 
 int main(int argc, char **argv){
@@ -26,9 +37,6 @@ int main(int argc, char **argv){
 	glutCreateWindow("Projecoes");
 	glutDisplayFunc(Desenha);
 	
-	
 	Inicializa();
 	glutMainLoop();	
 }
-
-
